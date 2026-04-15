8e0cb3e37387ace8f00333ceebf3317386a843f2
jexica ayran <jess.ayran@gmail.com>
2026-04-15 12:45:27 -0700

    corrected lesson 2

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -3,7 +3,7 @@ function TodoForm() {
     <>
       <form>
         <label htmlFor="todoTitle">Todo</label>
-        <input type="text" title="todoTitle" />
+        <input type="text" id="todoTitle" />
         <button type="submit" disabled>
           Add Todo
         </button>
