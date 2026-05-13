464f015c5b080e6b4d5979c468b741570af0da84
jexica ayran <jess.ayran@gmail.com>
2026-05-12 18:08:25 -0700

    TextInputWithLab el component updated

--- src/shared/TextInputWithLabel.jsx
+++ src/shared/TextInputWithLabel.jsx
@@ -1,8 +1,14 @@
-function TextInputWithLabel() {
+function TextInputWithLabel({ elementId, labelText, onChange, ref, value }) {
   return (
     <>
-      <label>Label Text</label>
-      <input type="text" />
+      <label htmlFor={elementId}>{labelText}</label>
+      <input
+        type="text"
+        id={elementId}
+        ref={ref}
+        value={value}
+        onChange={onChange}
+      />
     </>
   );
 }
