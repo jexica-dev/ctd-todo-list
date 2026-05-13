03cf3802aafb1384bc212f1966ade1fdd0e37317
jexica ayran <jess.ayran@gmail.com>
2026-05-12 19:16:29 -0700

    Toggle Between Todo Display and Edit

--- src/features/TodoList/TodoListItem.jsx
+++ src/features/TodoList/TodoListItem.jsx
@@ -1,12 +1,43 @@
-function TodoListItem({ todo, onCompleteTodo }) {
+import { useState } from 'react';
+import TextInputWithLabel from '../../shared/TextInputWithLabel.jsx';
+
+function TodoListItem({ todo, onCompleteTodo, onUpdateTodo }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [title, setTitle] = useState(todo.title);
+
+  const handleEditSubmit = (e) => {
+    e.preventDefault();
+    setIsEditing(false);
+  };
   return (
     <li>
-      <input
-        type="checkbox"
-        checked={todo.isCompleted}
-        onChange={() => onCompleteTodo(todo.id)}
-      />
-      {todo.title}
+      <form onSubmit={handleEditSubmit}>
+        {isEditing ? (
+          <TextInputWithLabel
+            labelText="Edit Todo"
+            elementId={`edit${todo.id}`}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        ) : (
+          <>
+            <label>
+              <input
+                type="checkbox"
+                id={`checkbox${todo.id}`}
+                checked={todo.isCompleted}
+                onChange={() => onCompleteTodo(todo.id)}
+              />
+            </label>
+            <span
+              onClick={() => setIsEditing(true)}
+              style={{ cursor: 'pointer' }}
+            >
+              {todo.title}
+            </span>
+          </>
+        )}
+      </form>
     </li>
   );
 }
