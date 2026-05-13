38d0e30f4e84539ae90cdb9f974e3a3cd91ae3c0
jexica ayran <jess.ayran@gmail.com>
2026-05-12 18:58:10 -0700

    refactor TodoForm to use TextInputWithLabel

--- src/features/TodoForm.jsx
+++ src/features/TodoForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import TextInputWithLabel from '../shared/TextInputWithLabel.jsx';
 
 function TodoForm({ onAddTodo }) {
   const [workingTodoTitle, setWorkingTodoTitle] = useState('');
@@ -18,16 +19,13 @@ function TodoForm({ onAddTodo }) {
   return (
     <>
       <form onSubmit={handleAddTodo}>
-        <label htmlFor="todoTitle">Todo</label>
-        <input
-          type="text"
-          id="todoTitle"
-          name="todoTitle"
-          placeholder="Todo text"
+        <TextInputWithLabel
+          elementId="todoTitle"
+          labelText="Todo"
           value={workingTodoTitle}
           onChange={(e) => setWorkingTodoTitle(e.target.value)}
-          required
         />
+
         <button type="submit" disabled={!workingTodoTitle.trim()}>
           Add Todo
         </button>
